7facf2b8ae280d22ad42adb6a876532b13e3fc82
Scherben <warn192444@gmail.com>
2022-04-28 19:03:16 +0700

    add transfer pet and get pet history function

--- contracts/Adoption.sol
+++ contracts/Adoption.sol
@@ -2,12 +2,14 @@ pragma solidity ^0.5.0;
 
 contract Adoption {
   address[16] public adopters;
+  mapping(uint => address[]) petHistory;
 
   // Adopting a pet
   function adopt(uint petId) public returns (uint) {
     require(petId >= 0 && petId <= 15);
 
     adopters[petId] = msg.sender;
+    petHistory[petId].push(msg.sender);
 
     return petId;
   }
@@ -15,5 +17,21 @@ contract Adoption {
   // Retrieving the adopters
   function getAdopters() public view returns (address[16] memory) {
     return adopters;
+  }
+
+  //transfer pet
+  function transfer(uint petId, address _target) public {
+    require(petId >= 0 && petId <= 15);
+    require(adopters[petId] == msg.sender);
+
+    adopters[petId] = _target;
+    petHistory[petId].push(_target);
+  }
+
+  //transfer pet
+  function getPetHistory(uint petId) public view returns (address[] memory) {
+    require(petId >= 0 && petId <= 15);
+
+    return petHistory[petId];
   }
 }
